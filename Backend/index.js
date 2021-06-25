d9ad71b711709c8c6ecd5af97a83982b2fe89903
Tobias Williams <towillia@redhat.com>
2021-06-25 13:15:53 -0400

    take-notes This updates the json

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const path = require('path');
+var data = {};
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello Worlds!')
 })
+
+app.get('/data/sample.json', (req, res) => {
+    res.sendFile(path.join(__dirname, '/data', 'sample.json'));
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
