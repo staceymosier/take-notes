3898f3e409940d4c3f987244d6cd9777b6f9afa1
Stacey <mosierdesign@gmail.com>
2021-06-25 08:37:08 -0700

    Add sample data

---
+++ Backend/index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    const method = req.method;
+    if (method == "POST") {
+        handlePost(req, res);
+    } else {
+        handlePut(req, res);
+    }
+});
+
+const handlePost = (req, res) => {}
+
+const handlePut = (req, res) => {}
+
+server.listen(8080);
